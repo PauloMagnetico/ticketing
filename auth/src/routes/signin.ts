ecdd400fc1d877d4df6b872f90d7037c9b8fb62d
PauloMagnetico <laurens.vangucht@outlook.com>
2024-01-17 22:32:42 +0100

    finished the npm module up and running

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,11 +1,11 @@
 import express, { Request, Response, json } from 'express';
 import { body } from 'express-validator';
 import  jwt  from 'jsonwebtoken';
+import { validateRequest, BadRequestError } from '@paulotickets/common';
 
 import { Password } from '../services/password';
 import { User } from '../models/user';
-import { validateRequest } from '../middlewares/validate-requests';
-import { BadRequestError } from '../errors/bad-request-error';
+
 
 
 const router = express.Router();
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,10 +1,9 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
-import { validateRequest } from '../middlewares/validate-requests';
+import { validateRequest, BadRequestError } from '@paulotickets/common';
 
 import { User } from '../models/user';
-import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 

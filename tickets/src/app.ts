2d8b4cd4fe4e7f23a3c4cbb473066bb52d03fa87
PauloMagnetico <laurens.vangucht@outlook.com>
2024-01-18 16:47:16 +0100

    fixed some auth references

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,10 +3,6 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 
-import { currentUserRouter } from './routes/currentuser';
-import { signInRouter } from './routes/signin';
-import { signOutRouter } from './routes/signout';
-import { signUpRouter } from './routes/signup';
 import { errorHandler, NotFoundError } from '@paulotickets/common';
 
 const app = express();
@@ -19,11 +15,6 @@ app.use(
         secure: process.env.NODE_ENV !== 'test'
     })
 );
-
-app.use(currentUserRouter);
-app.use(signInRouter);
-app.use(signOutRouter);
-app.use(signUpRouter);
 
 app.all('*', async (req, res) => {
     throw new NotFoundError();

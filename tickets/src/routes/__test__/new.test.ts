c7e691b6f13b5cd279c84ccffb9db9ebfb353e54
PauloMagnetico <laurens.vangucht@outlook.com>
2024-01-18 18:13:50 +0100

    fixed dependencies for testing

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,19 @@
+import request from 'supertest';
+import {app} from '../../app'
+import { unchangedTextChangeRange } from 'typescript';
+
+it('has a route handler listening to /api/tickets for post requests', async () => {
+
+});
+it('can only be accessed when signed in', async () => {
+
+});
+it('returns error when invalid title is provided', async () => {
+
+});
+it('returns error when invalid price', async () => {
+
+});
+it('creates a ticket when the input is valid', async () => {
+
+});

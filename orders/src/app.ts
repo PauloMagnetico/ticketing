48ec4fb63fb7cbf6fc5e76f29eb10dd2a04799c5
PauloMagnetico <laurens.vangucht@outlook.com>
2024-02-03 14:45:53 +0100

    added order service

---
+++ orders/src/app.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes';
+import { updateTicketRouter } from './routes/update';
+
+import { errorHandler, NotFoundError, currentUser } from '@paulotickets/common';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        //true in dev/prod but false in test, so we can read the cookie over http
+        secure: process.env.NODE_ENV !== 'test'
+    })
+);
+
+//currentUser needs to be set up after cookiesession
+app.use(currentUser)
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+app.all('*', async (req, res) => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ orders/src/index.ts
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
+
+const start = async () => {
+    if (!process.env.JWT_KEY) {
+        throw new Error('JWT_KEY must be defined')
+    }
+    if (!process.env.NATS_CLIENT_ID) {
+        throw new Error('NATS_CLIENT_ID must be defined')
+    }    
+    if (!process.env.NATS_URL) {
+        throw new Error('NATS_URL must be defined')
+    }    
+    if (!process.env.NATS_CLUSTER_ID) {
+        throw new Error('NQTS_CLUSTER_ID must be defined')
+    }    
+    if (!process.env.MONGO_URI) {
+        throw new Error('JMONGO_URI must be defined')
+    }    
+
+    try {
+        // we created the wrapper that returns ans instance
+        await natsWrapper.connect(
+            process.env.NATS_CLUSTER_ID,
+            process.env.NATS_CLIENT_ID,
+            process.env.NATS_URL
+        );
+ 
+        // we want to define the close here (not in the wrapper) 
+        // so there is no process exit all over the code
+        // in some hidden file
+        natsWrapper.client.on('close', () => {
+            console.log('NATS connection closed');
+            process.exit();
+        });
+
+        // mongoose keeps track of the connection internally
+        // so we can use this connection in other files
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('connected to mongoDB')
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+app.listen(3000, () => {
+    console.log('listening on port 3000 motherfucker')
+});
+
+start();

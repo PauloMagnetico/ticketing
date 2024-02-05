b645ef29390c860e75a417ea7e31bd4a99cb758c
PauloMagnetico <laurens.vangucht@outlook.com>
2024-02-05 10:33:16 +0100

    completed order routes

--- order/src/routes/__test__/new.test.ts
+++ order/src/routes/__test__/new.test.ts
@@ -52,3 +52,5 @@ it('reserves a ticket', async () => {
         .expect(201);
 });
 
+it.todo('emits an order created event');
+
--- order/src/routes/delete.ts
+++ order/src/routes/delete.ts
@@ -1,9 +1,23 @@
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from 'express';
+import { Order, OrderStatus } from '../models/order';
+import { NotAuthorizedError, NotFoundError, requireAuth } from '@paulotickets/common';
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId', async(req: Request, res: Response) => {
-    res.send({});
+// since we change status, maybe we should use patch instead of delete
+router.delete('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+    const order = await Order.findById(orderId);
+    if (!order) {
+        throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+        throw new NotAuthorizedError();
+    }
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    res.status(204).send(order);
 });
 
 export { router as deleteOrderRouter };
